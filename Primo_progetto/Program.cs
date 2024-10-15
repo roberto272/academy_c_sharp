efc7569942cabe66515f75aaf47c6b817ee493f2
Roberto Pipitone <roberto.pipitone.9673@gmail.com>
2024-10-15 17:52:25 +0200

    Primo commit

---
+++ Primo_progetto/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+#region Prova
+int i = 10;
+
+#if RELEASE
+i = 20;
+#endif
+
+for (int j = 1; j <= i; j++)
+{
+    Console.WriteLine(j.ToString());
+}
+#endregion
